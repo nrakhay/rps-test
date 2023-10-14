b0ee557deb6229ea3bba5441edba4e2469a7586d
nrakhay <nuralirakhay@gmail.com>
2023-10-14 11:32:12 +0600

    [back]: gamesession model

--- backend/base/models.py
+++ backend/base/models.py
@@ -11,3 +11,57 @@ class Player(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class GameSession(models.Model):
+    CHOICES = (
+        ("rock", "Rock"),
+        ("paper", "Paper"),
+        ("scissors", "Scissors"),
+    )
+    STATUS = (
+        ("waiting", "Waiting for opponent"),
+        ("in_progress", "In Progress"),
+        ("completed", "Completed"),
+    )
+
+    player1 = models.ForeignKey(
+        Player, related_name="player1_sessions", on_delete=models.CASCADE
+    )
+    player2 = models.ForeignKey(
+        Player,
+        related_name="player2_sessions",
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+    )
+    player1_choice = models.CharField(
+        max_length=10, choices=CHOICES, null=True, blank=True
+    )
+    player2_choice = models.CharField(
+        max_length=10, choices=CHOICES, null=True, blank=True
+    )
+    winner = models.ForeignKey(
+        Player,
+        related_name="winning_sessions",
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL,
+    )
+    status = models.CharField(max_length=20, choices=STATUS, default="waiting")
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def determine_winner(self):
+        if self.player1_choice == self.player2_choice:
+            return None
+        if (
+            (self.player1_choice == "rock" and self.player2_choice == "scissors")
+            or (self.player1_choice == "scissors" and self.player2_choice == "paper")
+            or (self.player1_choice == "paper" and self.player2_choice == "rock")
+        ):
+            return self.player1
+        return self.player2
+
+    def __str__(self):
+        return f"Game between {self.player1} and {self.player2}"
--- backend/base/serializer.py
+++ backend/base/serializer.py
@@ -14,3 +14,25 @@ class PlayerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Player
         fields = ("user", "name")
+
+
+class GameSessionSerializer(serializers.ModelSerializer):
+    player1_username = serializers.ReadOnlyField(source="player1.username")
+    player2_username = serializers.ReadOnlyField(
+        source="player2.username", default=None
+    )
+    winner_username = serializers.ReadOnlyField(source="winner.username", default=None)
+
+    class Meta:
+        model = GameSession
+        fields = (
+            "id",
+            "player1_username",
+            "player2_username",
+            "player1_choice",
+            "player2_choice",
+            "winner_username",
+            "status",
+            "created_at",
+            "updated_at",
+        )
