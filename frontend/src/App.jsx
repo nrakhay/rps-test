7ffbc9557352cdf11af951a0f3cd24928565c6cc
nrakhay <nuralirakhay@gmail.com>
2023-10-15 15:22:32 +0600

    [front]: socket setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 
+import React, { createContext, useState } from "react"
 import { AuthProvider } from './context/AuthContext'
 
 import Header from './components/Header'
@@ -9,20 +10,25 @@ import LoginPage from './pages/LoginPage'
 import GamePage from './pages/GamePage'
 import PrivateRoute from './utils/PrivateRoute'
 
+export const DataContext = createContext();
 
 function App() {
+  const [socket, setSocket] = useState(null);
+
   return (
     <div className="App">
-        <Router>
-            <AuthProvider>
-                <Header/>
-                <Routes>
-                    <Route path="/" element={<PrivateRoute><HomePage/></PrivateRoute>} />
-                    <Route path='/game' element={<PrivateRoute><GamePage/></PrivateRoute>}/>
-                    <Route path="/login" element={<LoginPage/>}/>
-                </Routes>
-            </AuthProvider>
-        </Router>
+        <DataContext.Provider value={{socket, setSocket }}>
+          <Router>
+              <AuthProvider>
+                  <Header/>
+                  <Routes>
+                      <Route path="/" element={<PrivateRoute><HomePage/></PrivateRoute>} />
+                      <Route path='/game' element={<PrivateRoute><GamePage/></PrivateRoute>}/>
+                      <Route path="/login" element={<LoginPage/>}/>
+                  </Routes>
+              </AuthProvider>
+          </Router>
+        </DataContext.Provider>
     </div>
   );
 }
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,15 +1,18 @@
-import React, { useContext, useEffect, useState } from 'react';
+import React, { useContext, useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import AuthContext from '../context/AuthContext';
 
 const HomePage = () => {
     const { authTokens, logoutUser, user } = useContext(AuthContext);
-    const [profile, setProfile] = useState([])
-    const navigate = useNavigate()
+    const [profile, setProfile] = useState([]);
+    const [waitingForOpponent, setWaitingForOpponent] = useState(false);
+    const navigate = useNavigate();
+    // const {socket, setSocket} = useContext(DataContext)
+    const socketRef = useRef()
 
     useEffect(() => {
-        getProfile()
-    },[])
+        getProfile();
+    }, []);
 
     const getProfile = async() => {
         let response = await fetch('http://127.0.0.1:8000/api/profile', {
@@ -28,17 +31,60 @@ const HomePage = () => {
         }
     }
 
-    const handleStart = () => {
-        navigate("/game")
-    }
+    const handleStart = async () => {
+        const response = await fetch(`http://127.0.0.1:8000/api/games/connect?username=${user.username}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + String(authTokens.access)
+            }
+        });
+
+        const data = await response.json();
+
+        const gameId = data.id.toString()
+
+        socketRef.current = new WebSocket(`ws://127.0.0.1:8000/ws/game/${data.id}/`);
+        // socketRef.current = new WebSocket(`ws://127.0.0.1:8000/ws/game/2/`);
+
+        socketRef.current.onopen = (e) => {
+            console.log('Connected to the WebSocket');
+        };
+      
+        socketRef.current.onmessage = (e) => {
+        const data = JSON.parse(e.data);
+        console.log(data);
+        };
+    
+        socketRef.current.onclose = (e) => {
+        console.error('WebSocket closed');
+        };
+    };
+
+    // const setupSocket = () => {
+    //     socket.onopen = (e) => {
+    //         console.log('Connected to the WebSocket');
+    //     };
+      
+    //     socket.onmessage = (e) => {
+    //     const data = JSON.parse(e.data);
+    //     console.log(data);
+    //     };
+    
+    //     socket.onclose = (e) => {
+    //     console.error('WebSocket closed');
+    //     };
+    // }
 
     return (
         <div>
             <p>Hello, {user.username}</p>
             <p>Let's start the game:</p>
-            <button onClick={handleStart}>Start</button>
+            
+            {!waitingForOpponent && <button onClick={handleStart}>Start</button>}
+            {waitingForOpponent && <p>Waiting for opponent...</p>}
         </div>
-    )
-}
+    );
+};
 
-export default HomePage
+export default HomePage;
